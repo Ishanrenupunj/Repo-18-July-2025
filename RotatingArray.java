446e34b613659e623d637b18697ae81c872cbdbb
Ishan <punjishanrenu@gmail.com>
2025-07-18 16:05:05 +0800

    how to display array

---
+++ RotatingArray.java
@@ -0,0 +1,9 @@
+public class RotatingArray {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7};
+        for(int a:arr){
+            System.out.print(a+",");
+        }
+    }
+    
+}
